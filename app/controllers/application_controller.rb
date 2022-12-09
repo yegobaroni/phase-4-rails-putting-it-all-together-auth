1daa16eae94dc4e26cf52ad89bd73496bbfd847c
yego <yegokipkemoi254@gmail.com>
2022-12-09 09:33:36 +0300

    partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+  skip_before_action :authorize, only: :create
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  private
+
+  def render_unprocessable_entity_response(valid)
+    render json: { errors: valid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def authorize
+    # using an instance method to be called on all need methods
+    @current_user = User.find_by(id: session[:user_id])
+    # render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
 end
